855d861cf441feebf8488b522601a08290f82b74
Tom Murphy <wolfmurphy@gmail.com>
2020-12-02 22:14:04 -0800

    AoC 2020: Day 3

---
+++ day03b.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+import re
+(xInc, tree) = (3, '#')
+def strip(s):
+    return list (s.strip())
+def doSlope(data, xInc, step):
+    (maxY, maxX, x, numTrees) = (len(data), len(data[0]), 0, 0)
+    for y in range(0, maxY, step):
+        numTrees += int(data[y][x] == tree)
+        x = (x + xInc) % maxX
+    return numTrees
+with open("day03a.txt",'r') as f:
+    data = list (map(strip, f.readlines()))
+    print(doSlope(data, 1, 1)*doSlope(data, 3, 1)*doSlope(data, 5, 1)*doSlope(data, 7, 1)*doSlope(data, 1, 2))
